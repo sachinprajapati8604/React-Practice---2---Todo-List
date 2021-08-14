493e920ffdc78214b59783dc47dff096727054b4
Sachin Prajapati <53299703+sachinprajapati8604@users.noreply.github.com>
2021-08-14 23:22:39 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,6 @@
 import React, { useState } from 'react';
 import './../styles/App.css';
 
-/**
-
-//! Make a button with id="btn" which on click adds the task to the list.
-
-Each item in the list should have a classname "list"
-
-Please ensure to render the task in the sequence they are added for example if a task "Buy milk" is added and then task "Buy vegetable" is added then they should be rendered in the same sequence first "Buy milk" and then "Buy vegetable".
-
-Each task should have edit and delete buttons corresponding to each task with class edit and delete respectively.
-
-There should be a textarea and save button corresponding to each task with class editTask and saveTask respectively which will be conditionally rendered when the edit button is clicked, please trigger onchange instead of using ref in editing task as test cases depend on it.
-
-The edited task should not be saved if the text area contains an empty string at this particular time save button should not be functional.
-
-On save the task should be updated and back to its normal state i.e edit and delete functionality is available.
-
-Initially, the task list should be empty.
-
-**Please use onChange instead of ref for controlled inputs as test cases depend on it.
- */
 function ID() {
   let id = 0;
   return function () {
